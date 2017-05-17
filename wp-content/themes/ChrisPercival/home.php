60a2f4911a01c9dbd49de68a6902e49763920515
Tommy Eagle <tomeagle79@gmail.com>
2017-05-17 10:35:08 +0100

    Standard update

--- wp-content/themes/ChrisPercival/home.php
+++ wp-content/themes/ChrisPercival/home.php
@@ -4,8 +4,6 @@
 
 <div class="main-blog main clearfix">
 
-	
-
 	<section class="first clearfix">
 		
 		<div>
@@ -42,6 +40,22 @@
 		<div class="blog-page-sidebar">
 			<div class="blog-recent-posts blog-border-box">
 				<h2>RECENT POSTS</h2>
+				<ul>
+				<!-- Define our WP Query Parameters -->
+				<?php $the_query = new WP_Query( 'posts_per_page=5' ); ?>
+
+				<!-- Start our WP Query -->
+				<?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
+
+				<!-- Display the Post Title with Hyperlink -->
+				<li><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></li>
+
+				<!-- Display the Post Excerpt -->
+				<li><?php // the_excerpt(__('(more…)')); ?></li>
+
+				<!-- Repeat the process and reset once it hits the limit -->
+				<?php endwhile;	wp_reset_postdata(); ?>
+				</ul>
 
 			</div>
 
