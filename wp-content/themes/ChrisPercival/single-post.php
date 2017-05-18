40d31370633c941671ed6d7187580043f3e02809
Tommy Eagle <tomeagle79@gmail.com>
2017-05-18 12:23:55 +0100

    updated navigation.php

--- wp-content/themes/ChrisPercival/single-post.php
+++ wp-content/themes/ChrisPercival/single-post.php
@@ -24,51 +24,14 @@
 						<p>DATE <?php the_time('Y'); ?></p>
 						<button>SHARE</button>
 						<p><?php the_content(); ?></p>
-
-
-
-												
+											
 					</div>
 					
 				</div>
 
 				<!--                        Custom post navigation                -->
-						<div id="cooler-nav" class="navigation">
-						<?php $prevPost = get_previous_post(true); if($prevPost) {?>
-
-						<div class="blog-border-box clearfix pag pag-left">
-							<div class="left">
-								<?php $prevthumbnail = get_the_post_thumbnail($prevPost->ID, array(100,100) );?>
-								<?php previous_post_link('%link',"<p>%title</p>", TRUE); ?>
-
-							</div>
-							<div class="right">
-
-								<?php previous_post_link('%link',"$prevthumbnail", TRUE); ?>
-							</div>
-						</div>
-
-
-
-
-						<?php } $nextPost = get_next_post(true);	if($nextPost) { ?>
-
-						<div class="blog-border-box clearfix pag pag-left" style="float:right;">
-							<div class="left">
-								<?php $nextthumbnail = get_the_post_thumbnail($nextPost->ID, array(100,100) );  ?>
-								<?php next_post_link('%link',"$nextthumbnail", TRUE); ?>
-							</div>
-							<div class="right">
-
-								<?php next_post_link('%link',"<p>%title</p>", TRUE); ?>
-							</div>
-						</div>
-
-						<?php } ?>
-						</div>
-
-					<!--#cooler-nav div -->
-
+			<?php get_template_part('template-parts/navigation','') ?>
+			
 			<?php endwhile; else : ?>
 					<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
 			<?php endif; ?>
